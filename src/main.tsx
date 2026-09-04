ba44be630704832bccd59ba07a9f661f9d81ffb2
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-04 00:27:56 -0700

    Enable resizable device table columns

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,9 @@ import './device-ux-polish.css';
 import './device-detail-v2.css';
 import './platform-intelligence-v2.css';
 import './sticky-device-header.css';
+import './resizable-columns.css';
 import './sticky-device-header';
+import './resizable-columns';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
