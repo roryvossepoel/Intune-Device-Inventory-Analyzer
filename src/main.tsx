ef17a2a4aae33909722fb9cef06a50d1f50fc047
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-09 14:19:10 +0200

    Use React-native users and ownership cards

--- src/main.tsx
+++ src/main.tsx
@@ -55,7 +55,6 @@ import './dashboard-unity-polish.css';
 import './mobile-hardware-icon-fix.css';
 import './mobile-hardware-unity.css';
 import './users-ownership.css';
-import './users-ownership';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
