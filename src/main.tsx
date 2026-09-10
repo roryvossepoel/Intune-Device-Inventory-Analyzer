b11288a86621d631d45de56480ca4c8d1a41a104
Rory Vossepoel <roryvossepoel@gmail.com>
2026-09-10 23:40:06 +0200

    Load product refresh styles

--- src/main.tsx
+++ src/main.tsx
@@ -55,6 +55,7 @@ import './dashboard-unity-polish.css';
 import './mobile-hardware-icon-fix.css';
 import './mobile-hardware-unity.css';
 import './users-ownership.css';
+import './product-refresh.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
